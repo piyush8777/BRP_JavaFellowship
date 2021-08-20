c1c719c54e6e22871519ae1b5dbbad36309f0000
Piyush Shaw <piyushshaw8777@gmail.com>
2021-08-21 02:28:11 +0530

    added:bootcamp day1 program

---
+++ bridgelabz/day1/PrintInitials.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.day1;
+
+public class PrintInitials {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println(" **     ***  **********      **             *             ** ");
+		System.out.println(" **    ***   **        **     **           ***           **  ");
+		System.out.println(" **   ***    **         **     **         ** **         **   ");
+		System.out.println(" **  ***     **          **     **       **   **       **    ");
+		System.out.println(" *****       **          **      **     **     **     **     ");
+		System.out.println(" **  ***     **          **       **   **       **   **      ");
+		System.out.println(" **   ***    **         **         ** **         ** **       ");
+		System.out.println(" **    ***   **        **           ***           ***        ");
+		System.out.println(" **     ***  **********              *             *         ");
+	}
+
+}
---
+++ bridgelabz/day1/SpringSeason.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.day1;
+
+public class SpringSeason {
+	public static void main(String[] args) {
+        
+        int Month = 0;
+		int date = 0;
+		if (Month <= 6 & date <= 20) {
+        	System.out.println(true);
+        }
+
+        else if (Month >= 3 & date<3 ) {
+        	System.out.println(true);
+        }
+
+        else {
+        	System.out.println(false);
+        }
+	}
+
+}

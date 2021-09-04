14f9300521bec2fd94487e29b27e9ce6cb8cd6b5
Piyush Shaw <piyushshaw8777@gmail.com>
2021-09-04 13:45:45 +0530

    added:bootcamp day11 program

---
+++ bridgelabz/day11/StdOut.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.day11;
+
+import org.w3c.dom.css.Counter;
+
+public interface StdOut {
+	static void println(Counter zipf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	static void printf(String string, int uniform) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	static void printf(String string, double gaussian) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	static void printf(String string, boolean bernoulli) {
+		// TODO Auto-generated method stub
+		
+	
+		
+	}
+
+	static void printf(String string, double sum1, double time1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	static void println(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+		
+	}
+
+
+
